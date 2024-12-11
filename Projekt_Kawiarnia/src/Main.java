eb06dd062a0f08567de302972e382c88ed6946f8
Hans Rudel <jakub.lubsz@gmail.con>
2024-12-11 20:16:28 +0100

    Ukończenie Projektu

--- Projekt_Kawiarnia/src/Main.java
+++ Projekt_Kawiarnia/src/Main.java
@@ -23,16 +23,23 @@ public class Main {
                 for(int i = 0; i < ilosc; i++)
                 {
 
-                    System.out.print("Nazwa produktu: ");
-                    String produkt = scanner.next();
-                    System.out.print("Czas Oczekiwania: ");
-                    int czas = scanner.nextInt();
-                    System.out.print("Cena: ");
-                    int cena = scanner.nextInt();
-                    System.out.print("Ocena produktu: ");
-                    String ocena = scanner.next();
+                    try {
 
-                    opcje[i] = new Menuu(produkt, czas, cena, ocena);
+                        System.out.print("Nazwa produktu: ");
+                        String produkt = scanner.next();
+                        System.out.print("Czas Oczekiwania: ");
+                        int czas = scanner.nextInt();
+                        System.out.print("Cena: ");
+                        int cena = scanner.nextInt();
+                        System.out.print("Ocena produktu: ");
+                        String ocena = scanner.next();
+
+                        opcje[i] = new Menuu(produkt, czas, cena, ocena);
+
+                    } catch (Exception e)
+                    {
+                        System.out.println("Podaj Poprawne Dane!");
+                    }
 
                 }
             }
